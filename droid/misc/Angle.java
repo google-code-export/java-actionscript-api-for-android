3838d957c09eb9784ce6e2cda2fc54371d360113
mjmendoza@ymail.com <mjmendoza@ymail.com@b01686f8-1bf4-11df-b461-89744e4a0792>
2010-02-12 07:16:04 +0000

    initial commit

---
+++ droid/misc/Angle.java
@@ -0,0 +1,60 @@
+/**
+* Jasafa - Java ActionScript API For Android
+* Java implementation of ActionScript3
+*
+* MIT Licensed (http://www.opensource.org/licenses/mit-license.php)
+* Copyright (c) 2010 Mj Mendoza IV <mjmendoza@konsolscript.org>
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*/
+package droid.misc;
+
+import droid.geom.Point;
+
+/**
+ * Not originally an ActionScript class
+ * @author Mj Mendoza IV
+ * 
+ * The Angle class contains reusable functions for calculating degrees in Radians or Degrees unit.
+ */
+public class Angle {
+	public static double getRadians(double degrees) {
+		return degrees / (180 / Math.PI);
+	}
+	public static double getDegrees(double radians) {
+		return radians / (Math.PI / 180);
+	}
+	public static double getAngleByRadians(double distanceX, double distanceY) {
+		return Math.atan2(distanceX, distanceY);
+	}
+	public static double getAngleByDegrees(double distanceX, double distanceY) {
+		return Angle.getDegrees(Angle.getAngleByRadians(distanceX, distanceY));
+	}
+	/**
+	 * Returns a point from originPt to an angled distance.
+	 * @param	originPt    Point of origin
+	 * @param	distance    Distance from point of origin
+	 * @param	angle       Angle in radians
+	 * @return
+	 */
+	public static Point getPointByAngledDistance(Point originPt, double distance, double angle) {
+		Point tmp = Point.polar(distance, angle);
+		return new Point(originPt.x + tmp.x, originPt.y + tmp.y);
+	}
+}
